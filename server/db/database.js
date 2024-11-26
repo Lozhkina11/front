c055de6d68537860ba0d27d6c3a9069524286cf7
igor <igor932010@mail.ru>
2024-11-26 19:12:13 +0300

    added db

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   development: { 
-   username: 'postgres', 
-   password: '081099', 
-   database:  process.env.DB_NAME, 
-   host: '127.0.0.1', 
-   dialect: 'postgres', 
+    username: 'admin', //process.env.DB_USER,
+    password:'123',           // process.env.DB_PASS,
+    database:"photoshare", //process.env.DB_NAME,
+    host: process.env.DB_HOST, //
+    dialect: process.env.DB_DIALECT || 'postgres',
 },
   test: {
     username: "root",
