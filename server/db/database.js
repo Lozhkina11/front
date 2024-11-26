1652adc4c726dec51556231f478709d78987bd92
Lozhkina Iuliana <lozhkina_2021@list.ru>
2024-11-26 19:13:58 +0300

    changed settings conection with db

--- server/db/database.js
+++ server/db/database.js
@@ -1,11 +1,11 @@
 module.exports = {
-  development: { 
-   username: 'postgres', 
-   password: '081099', 
-   database:  process.env.DB_NAME, 
-   host: '127.0.0.1', 
-   dialect: 'postgres', 
-},
+  development: {
+    username:"postgres",
+    password: "081099",
+    database: "photoshare",
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "postgres",
+  },
   test: {
     username: "root",
     password: null,
